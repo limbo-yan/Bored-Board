a8268132b96f16c6badbcfae866d50719e279e7c
Yan <limboyan0622@gmail.com>
2022-11-03 14:44:40 -0400

    Added comments to code

--- Prototype_arduino/prototype_33_neopixel.ino
+++ Prototype_arduino/prototype_33_neopixel.ino
@@ -1,6 +1,5 @@
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
 #define PIN  6
@@ -8,38 +7,46 @@
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
-
+// Pins for rows and cols of switches
 int col[] = {0, 1, 2};
 int row[] = {3, 4, 5};
 
+int rowSize = sizeof(row) / sizeof(row[0]);
+int colSize = sizeof(col) / sizeof(col[0]);
 
 void setup() {
- for (int i = 0; i < sizeof(col) / sizeof(col[0]); i ++) {
-  pinMode(col[i], INPUT);
- }
- for (int i = 0; i < sizeof(row) / sizeof(row[0]); i ++) {
-  pinMode(row[i], OUTPUT);
- }
- pixels.begin(); 
- pixels.clear();
- pixels.show();
+  // Set all columns as input
+  for (int i = 0; i < colSize; i ++) {
+    pinMode(col[i], INPUT);
+  }
+  // Set all rows as output
+  for (int i = 0; i < rowSize; i ++) {
+    pinMode(row[i], OUTPUT);
+  }
+
+  pixels.begin(); 
+  pixels.clear();
+  pixels.show();
 }
 
 void loop() {
-  for (int i = 0; i < sizeof(row) / sizeof(row[0]); i ++) {
+  for (int i = 0; i < rowSize; i ++) {
+    // For each iteration, set each row as LOW first
     digitalWrite(row[i], LOW);
-    for (int j = 0; j < sizeof(col) / sizeof(col[0]); j ++) {
+    for (int j = 0; j < colSize; j ++) {
+      // Read every column's pin to check if the switch is pressed or not
       int buttonState = digitalRead(col[j]);
-      int pixelIdx = i * 3 + j;
+      // Convert the switch location to LED's location
+      int pixelIdx = i * rowSize + j;
       if (buttonState == LOW) {
-        pixels.setPixelColor(pixelIdx, pixels.Color(0, 150, 0));
-        pixels.show();
-      } else {
-        pixels.setPixelColor(pixelIdx, pixels.Color(150, 0, 0));
-        pixels.show();
+        // Light up the specific LED when the switch is pressed
+        if(pixels.getPixelColor(pixelIdx) == 0x000000) {
+          pixels.setPixelColor(pixelIdx, pixels.Color(0, 150, 0));
+        }
       }
-  }
-  digitalWrite(row[i], HIGH);
+      pixels.show();
+    }
+    // Set this row as HIGH and continue to check another row
+    digitalWrite(row[i], HIGH);
   }
 }
