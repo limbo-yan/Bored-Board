458300f6450e5f63d5064a1c028525e67ac05baa
Yan <limboyan0622@gmail.com>
2022-11-30 16:53:20 -0500

    PvP works for 8*8

--- Gomoku/alphaZero/human_play.py
+++ Gomoku/alphaZero/human_play.py
@@ -1,11 +1,10 @@
 from __future__ import print_function
-import pickle
 from game import Board, Game
 from mcts_pure import MCTSPlayer as MCTS_Pure
 from mcts_alphaZero import MCTSPlayer
-from policy_value_net_numpy import PolicyValueNetNumpy
+# from policy_value_net_numpy import PolicyValueNetNumpy
 # from policy_value_net import PolicyValueNet  # Theano and Lasagne
-# from policy_value_net_pytorch import PolicyValueNet  # Pytorch
+from policy_value_net_pytorch import PolicyValueNet  # Pytorch
 # from policy_value_net_tensorflow import PolicyValueNet # Tensorflow
 # from policy_value_net_keras import PolicyValueNet  # Keras
 
@@ -56,8 +55,8 @@ def run():
     n = 5
     width, height = 8, 8
     # model_file = 'best_policy_3_3_3_pytorch100.model'
-    # model_file = 'best_policy_8_8_5_pytorch950.model'
-    model_file = 'best_policy_8_8_5.model'
+    model_file = 'best_policy_8_8_5_pytorch950.model'
+    # model_file = 'best_policy_8_8_5.model'
     try:
         board = Board(width=width, height=height, n_in_row=n)
         game = Game(board, arduino)
@@ -65,11 +64,11 @@ def run():
         # ############### human VS AI ###################
         # load the trained policy_value_net in either Theano/Lasagne, PyTorch or TensorFlow
 
-        # best_policy = PolicyValueNet(width, height, model_file = model_file)
-        # mcts_player = MCTSPlayer(best_policy.policy_value_fn, c_puct=5, n_playout=400)
+        best_policy = PolicyValueNet(width, height, model_file = model_file)
+        mcts_player = MCTSPlayer(best_policy.policy_value_fn, c_puct=5, n_playout=400)
 
         # load the provided model (trained in Theano/Lasagne) into a MCTS player written in pure numpy
-        # '''
+        '''
         try:
             policy_param = pickle.load(open(model_file, 'rb'))
         except:
@@ -79,18 +78,19 @@ def run():
         mcts_player = MCTSPlayer(best_policy.policy_value_fn,
                                  c_puct=5,
                                  n_playout=400)  # set larger n_playout for better performance
-        # '''
+        '''
         # uncomment the following line to play with pure MCTS (it's much weaker even with a larger n_playout)
         # mcts_player = MCTS_Pure(c_puct=5, n_playout=1000)
 
         # human player, input your move in the format: 2,3
-        human = Human(arduino)
+        human1 = Human(arduino)
+        human2 = Human(arduino)
 
         # set start_player=0 for human first
-        game.start_play(human, mcts_player, start_player=0, is_shown=1)
+        # game.start_play(human1, mcts_player, start_player=1, is_shown=1)
 
         # two players
-        # game.start_play(human, human, start_player=0, is_shown=1)
+        game.start_play(human1, human2, start_player=0, is_shown=1)
         
     except KeyboardInterrupt:
         print('\n\rquit')
